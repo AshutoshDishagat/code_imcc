058d5f53dec83e8fc8af905cf7fc4b0c8b24fb13
AshutoshDishagat <dishagatashutosh@gmail.com>
2026-09-10 21:17:07 +0530

    Python codes3

--- test.py
+++ test.py
@@ -14,13 +14,22 @@
 # print(f" {n} years = {n * 365} days")
 
 # n = int(input("Enter a number: "))
-# a = n//60
-# b = n%60
-# print(f"{n} is {a} hours {b} minutes")
+# print(f"{n} is {n//60} hours {n%60} minutes")
 
 
-n = int(input("Enter a number: "))
-print(f"{n} : last no. is {n%10}")
+# n = int(input("Enter a number: "))
+# print(f"{n} : last no. is {n%10}")
 
     
+# Age = int(input("Enter a number: "))
+# Occupation = input("Enter a Occupation: ") 
+# eligible = (Occupation == "student") and (Age < 21)
+# print("Eligible :" , eligible)
 
+n = int(input("Enter a number: "))
+m = int(input("Enter a number: "))
+
+n = n + m
+m = n - m   
+n = n - m
+print("After swapping: n =", n, "m =", m)
