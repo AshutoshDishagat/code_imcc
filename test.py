a0629be6c27d86b800b229c6f57d8130ac555e38
AshutoshDishagat <dishagatashutosh@gmail.com>
2026-09-10 15:09:41 +0530

    Python codes

---
+++ test.py
@@ -0,0 +1,17 @@
+# a = int(input("Enter a value: "))
+# b=10
+# c = a+b
+# print("The sum of a and b is:", c)
+
+# print("this is an even number" if int(input("Enter another value: ")) % 2 == 0 else "this is an odd number")\
+
+
+# n = int(input("Enter a number: "))
+# print( "Number is even" , n % 2 != 0)
+
+
+n = int(input("Enter a number: "))
+print(f" {n} years = {n * 365} days")
+
+
+
